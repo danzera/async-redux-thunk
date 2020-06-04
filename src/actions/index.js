b9933203aba3d6474d91a272c7707157c45b7326
Dan Zera <dczera@gmail.com>
2020-06-03 23:31:38 -0500

    add api file for creating axios request to the json-placeholder api, demonstrate how NOT to use such a function in an action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,13 @@
-export const fetchPosts = () => {
-	console.log('fetchPosts')
+import jsonPlaceholder from '../apis/json-placeholder';
+
+export const fetchPosts = async () => {
+	// bad approach - breaks the rules of action creators with Redux
+	// Babel transpiles this function into different looking code
+	// aync-await returns a request object the first time the action creator is called
+	const response = await jsonPlaceholder.get('/posts');
+
 	return {
-		type: 'FETCH_POSTS'
+		type: 'FETCH_POSTS',
+		payload: response
 	};
 };

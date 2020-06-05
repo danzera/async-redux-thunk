9b2b9cbcda0b43d3f391df1b8f972caca9dcadbb
Dan Zera <dczera@gmail.com>
2020-06-05 15:28:50 -0500

    add skeleton for posts reducer

---
+++ src/reducers/posts-reducer.js
@@ -0,0 +1,11 @@
+// reducers must return a value other than 'undefined'
+// and they produce 'state' (data) using only previous state and the action
+// reducers are pure ==> must not reach 'out of itself' to decide what value to return
+// e.g. they should not be making API requests, read file info or solicit user/DOM for input
+// and they must not mutate its input 'state' argument
+export default (state, action) => {
+	console.log('posts-reduer')
+	// reducers must have a default return statement
+	// if the state passed to the reducer is undefined, you must explicitly return the initial state
+	return 123;
+};

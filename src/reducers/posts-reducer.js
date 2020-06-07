d433601e5db43c77ae0b9f3664d13cf97543590d
Dan Zera <dczera@gmail.com>
2020-06-06 23:51:48 -0500

    update comments regarding state changes with reducers

--- src/reducers/posts-reducer.js
+++ src/reducers/posts-reducer.js
@@ -3,6 +3,7 @@
 // reducers are pure ==> must not reach 'out of itself' to decide what value to return
 // e.g. they should not be making API requests, read file info or solicit user/DOM for input
 // and they must not mutate its input 'state' argument
+// in actuality 'state' can be mutated, BUT Redux may not register it as a state change and if so does not forward the state change onto the rest of the application
 export default (state, action) => {
 	console.log('posts-reduer')
 	// reducers must have a default return statement

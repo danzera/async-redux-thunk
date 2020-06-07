7acd4e5c0fbf381a070be3dbea1243d3c4d69309
Dan Zera <dczera@gmail.com>
2020-06-07 00:05:49 -0500

    posts-reducer using if statement prior to using switch statement

--- src/reducers/posts-reducer.js
+++ src/reducers/posts-reducer.js
@@ -4,9 +4,15 @@
 // e.g. they should not be making API requests, read file info or solicit user/DOM for input
 // and they must not mutate its input 'state' argument
 // in actuality 'state' can be mutated, BUT Redux may not register it as a state change and if so does not forward the state change onto the rest of the application
-export default (state, action) => {
-	console.log('posts-reduer')
+
+// state will be a list of records eventually, so default state to an empty array 
+export default (state = [], action) => {
+	if (action.type === 'FETCH_POSTS') {
+		return action.payload;
+	}
+
+	// just return the previous state if the action type does not match
 	// reducers must have a default return statement
 	// if the state passed to the reducer is undefined, you must explicitly return the initial state
-	return 123;
+	return state;
 };

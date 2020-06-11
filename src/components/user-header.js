593430d3b07260187b52cf4b000a8c7e4b5b4a3a
Dan Zera <dczera@gmail.com>
2020-06-11 09:06:23 -0500

    refactor user-header component so it only receives the applicable user data for an individual user

--- src/components/user-header.js
+++ src/components/user-header.js
@@ -8,23 +8,26 @@ class UserHeader extends React.Component {
 	}
 
 	render () {
-		// search for user in the users array
-		const user = this.props.users.find(user => user.id === this.props.userId);
-
+		const { user } = this.props;
 		// don't show anything if there is no user
 		if (!user) return null;
 
 		return (
 			<div className="user-header header">
-				{user.name}
+				{ user.name }
 			</div>
 		);
 	}
 }
 
 // any time we want data in our React component from the Redux side of our app, we define a mapStateToProps() function
-const mapStateToProps = state => {
-	return { users: state.users };
+// mapStateToProps receives a reference to the component's props as a second arg
+const mapStateToProps = (state, ownProps) => {
+	// search for user in the users array
+	// best to do this here in the mapStateToProps function since our component only needs access to one user
+	// improves component reusability and allows use to factor out the mapStateToProps function to a separate file, if desired
+	const user = state.users.find(user => user.id === ownProps.userId);
+	return { user };
 }
 
 // mapStateToProps gets passed off to the connect() function
